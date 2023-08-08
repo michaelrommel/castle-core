f89fdc4e1b03ddf65d02ff43a7589a2b45134019
Michael Rommel <rommel@layer-7.net>
2023-08-08 13:15:00 +0200

    feat: add update_rtx script with fallback

--- home/bin/update_rtx.sh
+++ home/bin/update_rtx.sh
@@ -22,18 +22,25 @@ get_arch() {
 	fi
 }
 
+# https://github.com/jdxcode/rtx/releases/download/v1.35.8/rtx-v1.35.8-linux-x64
 update_rtx() {
 	echo "Updating rtx..."
 	os="$(get_os)"
 	if [[ "${os}" == "macos" ]]; then
 		brew install rtx
-		RTX=rtx
 	else
 		arch="$(get_arch)"
 		mkdir -p "${HOME}/bin"
-		curl -sL "https://rtx.pub/rtx-latest-${os}-${arch}" >"${HOME}/bin/rtx"
+		if ! curl -sL "https://rtx.pub/rtx-latest-${os}-${arch}" >"${HOME}/bin/rtx"; then
+			echo "First download failed."
+			latest=$(curl -s https://api.github.com/repos/jdxcode/rtx/tags | jq -r ".[0].name")
+			echo "Latest release seems to be: ${latest}"
+			if ! curl -sL "https://github.com/jdxcode/rtx/releases/download/${latest}/rtx-${latest}-${os}-${arch}" >"${HOME}/bin/rtx"; then
+				echo "Second download failed, too. Aborting."
+				exit 1
+			fi
+		fi
 		chmod 755 "${HOME}/bin/rtx"
-		RTX="${HOME}/bin/rtx"
 	fi
 	echo "done."
 }
