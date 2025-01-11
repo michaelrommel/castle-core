b9f201a4ba6fb01c1a1a19351360bb76a17708a5
Michael Rommel <rommel@layer-7.net>
2025-01-11 15:13:26 +0100

    fix: link to config, fix shell function

--- home/.path.d/80_yazi.sh
+++ home/.path.d/80_yazi.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
 function y() {
-	local tmp
-	tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	local tmp cwd
+	tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
 	yazi "$@" --cwd-file="$tmp"
 	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
 		builtin cd -- "$cwd" || exit
