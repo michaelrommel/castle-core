2803c3bfce58ddb096b31b445be3ea8740ebb28d
Michael Rommel <rommel@layer-7.net>
2025-01-11 15:08:23 +0100

    feat: add yazi

---
+++ home/.path.d/80_yazi.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+function y() {
+	local tmp
+	tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd" || exit
+	fi
+	rm -f -- "$tmp"
+}
