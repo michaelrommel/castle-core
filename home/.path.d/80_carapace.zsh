4f32961e670c70e227aae9c1f5c7dc5091fa49f3
Michael Rommel <rommel@layer-7.net>
2025-05-03 22:13:36 +0200

    fix: simplify carapace messages, add early compinit

--- home/.path.d/80_carapace.zsh
+++ home/.path.d/80_carapace.zsh
@@ -3,7 +3,8 @@
 RET=$(carapace --version 2>/dev/null)
 if [[ "$?" -eq 0 ]]; then
 	export CARAPACE_BRIDGES='zsh,bash'
-	zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
+	zstyle ':completion:*' format $'\e[2;37m%d\e[m'
 	zstyle ':completion:*:git:*' group-order 'main commands' 'alias commands' 'external commands'
+	autoload -Uz compinit && compinit
 	source <(carapace _carapace)
 fi
