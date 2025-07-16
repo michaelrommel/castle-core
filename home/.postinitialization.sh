910c01cdf0de97dabf73a78c9b5e19dcabfc7285
Michael Rommel <rommel@layer-7.net>
2025-07-16 17:21:57 +0200

    fix: proper env setting for LS and wezterm config for windows

--- home/.postinitialization.sh
+++ home/.postinitialization.sh
@@ -17,13 +17,15 @@ alias grep='grep --colour=auto'
 
 if [[ "${OSNAME}" == "Darwin" ]]; then
 	LS='gls'
+else
+	LS='ls'
 fi
 if type l 2>/dev/null 1>&2; then
 	unalias 'l'
 fi
-alias l="${LS} -lah --color --hyperlink=auto"
-alias ll="${LS} -lah --color --hyperlink=auto"
-alias lr="${LS} -lahtr --color --hyperlink=auto"
+alias l="${LS} -lah --color --hyperlink=never"
+alias ll="${LS} -lah --color --hyperlink=never"
+alias lr="${LS} -lahtr --color --hyperlink=never"
 
 lb() {
 	${LS} -lah --color=always "$@" | bat
