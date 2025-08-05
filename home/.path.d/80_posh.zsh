41e0dc03a24c7c6dd03667c9ce9207d961faa4d5
Michael Rommel <rommel@layer-7.net>
2025-08-05 14:02:15 +0200

    fix: add bob to path, remove posh

--- home/.path.d/80_posh.zsh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env zsh
-
-if [[ -d "${HOME}/.oh-my-posh" && ! ":${PATH}:" == *:${HOME}/.oh-my-posh:* ]]; then
-	# path has not yet been added
-	export PATH="${HOME}/.oh-my-posh:${PATH}"
-fi
